6edf62a3265dc89adf8d6bf866c0a0b085a368dd
Ajaezo Kingsley <ajaezokingsley@gmail.com>
2022-09-14 21:43:20 -0700

    particles

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -62,8 +62,8 @@ const Navbar: React.FC<{}> = () => {
         )}
         {toggleMenu && (
           <ul
-            className="z-10 fixed -top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none
-            flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in"
+            className="z-50 fixed -top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none
+            flex flex-col justify-start items-end rounded-md bg-black text-white animate-slide-in"
           >
             <li className="text-xl w-full my-2">
               <AiOutlineClose onClick={() => setToggleMenu(false)} />
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,41 +1,12 @@
 import React, { useCallback } from 'react';
 import ReactDOM from 'react-dom/client';
 import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import App from './App';
 import { TransactionProvider } from './context/TransactionsContext';
-import 'react-toastify/dist/ReactToastify.css';
+
 import './index.css';
-import Particles from 'react-tsparticles';
-import { loadFull } from 'tsparticles';
-import { Container } from 'react-dom';
 import ParticlesBackground from './components/ParticlesBackground';
-
-// interface CropperFix extends React.Component {}
-
-// const Cropped = (Cropper as any) as {
-//     new(): CropperFix;
-// };
-
-// const props: any = {
-//     cropShape: "round",
-//     disableAutomaticStylesInjection: true,
-//     image,
-//     crop,
-//     zoom,
-//     aspect: 1,
-//     onCropChange: setCrop,
-//     onZoomChange: setZoom,
-//     onCropComplete: onCropComplete,
-// }
-
-// ...
-
-// const props = {
-//   id: 'tsparticles',
-//   url: 'http://foo.bar/particles.json',
-//   init: { particlesInit },
-//   loaded: { particlesLoaded },
-// };
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <TransactionProvider>
